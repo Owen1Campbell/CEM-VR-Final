762103a792ed11e350e5c519be748e70e3066bf5
Nathan <nathanrodriguez240@gmail.com>
2024-11-12 21:01:39 -0500

    taps and anthem added

--- Unity Project/Giroux-CAP6117-SANC - crown/Assets/Scripts/AmbientAudioController.cs
+++ Unity Project/Giroux-CAP6117-SANC - crown/Assets/Scripts/AmbientAudioController.cs
@@ -7,17 +7,54 @@ public class AmbientAudioController : MonoBehaviour
     private GameObject day;
     private GameObject night;
     private GameObject rain;
+    private GameObject taps;
+    private GameObject anthem;
 
     private bool isDaytime;
     private bool isRaining;
+    private bool isTapsActive;
+    private bool isAnthemActive;
     // Start is called before the first frame update
     void Start()
     {
         isDaytime = true;
         isRaining = false;
+        isTapsActive = false;
+        isAnthemActive = false;
         day = transform.GetChild(0).gameObject;
         night = transform.GetChild(1).gameObject;
         rain= transform.GetChild(2).gameObject;
+        taps = transform.GetChild(3).gameObject;
+        anthem = transform.GetChild(4).gameObject;
+    }
+
+    public void ToggleTaps()
+    {
+        if(isTapsActive)
+        {
+              taps.SetActive(false);
+              isTapsActive =  false;
+        }
+          
+        else
+        {
+            isTapsActive = true;
+            taps.SetActive(true);
+        }
+    }
+
+    public void ToggleAnthem()
+    {
+        if(isAnthemActive)
+        {
+            anthem.SetActive(false);
+            isAnthemActive = false;
+        }
+
+        else{
+            isAnthemActive =  true;
+            anthem.SetActive(true);
+        }
     }
 
     public void ToggleRain()
